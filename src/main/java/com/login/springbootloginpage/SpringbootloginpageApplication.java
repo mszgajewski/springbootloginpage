e686da76723345f3db78fcc7e0ae5f6e8edd8438
mszgajewski <mszgajewski@gmail.com>
2021-07-25 17:18:21 +0200

    Initial commit

---
+++ src/main/java/com/login/springbootloginpage/SpringbootloginpageApplication.java
@@ -0,0 +1,13 @@
+package com.login.springbootloginpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootloginpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootloginpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/login/springbootloginpage/SpringbootloginpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.springbootloginpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootloginpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
